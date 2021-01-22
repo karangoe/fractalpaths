eeeec7c6d301c7561da62c2d3e3ec4160d5d9b98
karangoe <elenekarangozishvili@gmail.com>
2021-01-22 01:57:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fractalpaths", # Replace with your own username
+    name="fractalpaths",
     version="0.0.1",
     author="Elene Karangozishvili",
     author_email="elenekarangozishvili@gmail.com",
     description="Given a fractal and two points in the fractal, finds the shortest taxicab path between the points that stays in the fractal",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/karangoe/fractalpaths",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
